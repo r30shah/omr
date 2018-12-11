ddaff09ddfc7e9a213a825c214caa20e7921288c
Rahil Shah <rahil@ca.ibm.com>
2018-12-11 15:57:28 -0500

    Abstract Method Inlining
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- compiler/z/codegen/ControlFlowEvaluator.cpp
+++ compiler/z/codegen/ControlFlowEvaluator.cpp
@@ -173,6 +173,7 @@ virtualGuardHelper(TR::Node * node, TR::CodeGenerator * cg)
          case TR_DirectMethodGuard:
          case TR_NonoverriddenGuard:
          case TR_InterfaceGuard:
+         case TR_AbstractGuard:
          case TR_MethodEnterExitGuard:
          case TR_HCRGuard:
          //case TR_AbstractGuard:
