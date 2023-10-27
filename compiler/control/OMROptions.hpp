14072c8535bfd5169361cdda89d8bbf63f90c554
Rahil Shah <rahil@ca.ibm.com>
2023-10-27 12:01:43 -0400

    Add lastOpt and lastOptSub index setters

--- compiler/control/OMROptions.hpp
+++ compiler/control/OMROptions.hpp
@@ -1633,6 +1633,8 @@ public:
    int32_t   getFirstOptIndex()                {return _firstOptIndex;}
    int32_t   getLastOptIndex()                 {return _lastOptIndex;}
    int32_t   getLastOptSubIndex()              {return _lastOptSubIndex;}
+   void      setLastOptIndex(int32_t lastOptIndex) { _lastOptIndex = lastOptIndex; }
+   void      setLastOptSubIndex(int32_t lastOptSubIndex) { _lastOptSubIndex = lastOptSubIndex; }
    int32_t   getLastIpaOptTransformationIndex() {return _lastIpaOptTransformationIndex;}
    int32_t   getNumInterfaceCallCacheSlots()     {return _numInterfaceCallCacheSlots;}
    int32_t   getNumInterfaceCallStaticSlots()    {return _numInterfaceCallStaticSlots;}
